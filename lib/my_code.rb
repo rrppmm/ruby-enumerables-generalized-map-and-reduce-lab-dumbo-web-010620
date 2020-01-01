0086c624c826b77fb3d8e723bc3483fb408cc0e3
Paul M <paulmcadory@gmail.com>
2020-01-01 21:40:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(array, starting_value = nil)
+  if starting_value
+    sum = starting_value
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  
+  while i < array.length do
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
